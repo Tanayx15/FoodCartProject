ee24ad3a8e2d071473d626e0bb987e29a4ee2b26
Tanayx15 <141574343+Tanayx15@users.noreply.github.com>
2024-09-11 22:02:57 +0530

    Create InsertNewCartItem.cs

---
+++ InsertNewCartItem.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using Moq;
+using FoodCart_Hexaware.Data;
+using FoodCart_Hexaware.Models; 
+using System.Threading.Tasks;
+
+[TestFixture]
+public class CartItemRepositoryTests
+{
+    private Mock<IRepository<CartItem>> _cartItemRepositoryMock;
+    private CartItem _testCartItem;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _cartItemRepositoryMock = new Mock<IRepository<CartItem>>();
+        _testCartItem = new CartItem { Id = 1, MenuItemId = 1, Quantity = 2 };
+    }
+
+    [Test]
+    public async Task InsertCartItem_ShouldAddCartItem_WhenValidCartItemProvided()
+    {
+        // Arrange
+        _cartItemRepositoryMock.Setup(repo => repo.InsertAsync(It.IsAny<CartItem>()))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        await _cartItemRepositoryMock.Object.InsertAsync(_testCartItem);
+
+        // Assert
+        _cartItemRepositoryMock.Verify(repo => repo.InsertAsync(It.Is<CartItem>(c => c.MenuItemId == _testCartItem.MenuItemId)), Times.Once);
+    }
+}
